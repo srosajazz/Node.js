928f0e26275e53972a02c72b72c9c759d0d86dcb
Sergio Rosa <scarlosjazz@gmail.com>
2019-02-26 03:12:41 -0500

    Laptop Store Project start

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+    const pathName = url.parse(req.url, true).pathname;
+    const id= url.parse(req.url, true).id;
+
+    console.log(url.parse(req.url, true));
+
+    if(pathName === '/products' || pathName ==='/') {
+        res.writeHead(200, { 'content-type': 'text/html'});
+        res.end('This is the Product page!');
+    }
+    else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'content-type': 'text/html'});
+        res.end(`This is the LAPTOP page for laptop ${id}!`);
+    }
+    else {
+        res.writeHead(404, { 'content-type': 'text/html'});
+        res.end('URL was not found on the server!');
+    }
+});
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for request now');
+});
+
